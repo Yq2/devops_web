88bef4c8566a31ec1279d2e7b43301708cf7b01d
Yq2 <1225807604@qq.com>
2018-08-26 12:51:08 +0800

    devops_web v1.0.9

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,15 @@
 
 kill -9 $(pgrep webserver)
 
-cd /opt/go/gopath/src/github.com/Yq2/devops_web/
+cd /opt/go/gopath/src/github.com/Yq2/
 
 git clone https://github.com/Yq2/devops_web.git
 
+cd devops_web/
+
 cd ./webserver/
+
+rm -rf webserver
 
 go build main.go
 
